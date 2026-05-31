0b9959cf69c38fcffe13a295dbdb15b1702c20db
Michael Talapila <11203122+mikhailtalapila@users.noreply.github.com>
2026-05-30 20:14:53 -0400

    Add square root problem

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-const contacts = [
-  "Chris:1233452342",
-  "Sarah:1234342561",
-  "Bill:4322345632",
-  "Mary:2512342345",
-  "Dianne:123423452",
-];
-
 const para = document.querySelector("p");
 const input = document.querySelector("input");
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", () => {
-  const searchName = input.value.toLowerCase();
+  para.textContent = "Output: ";
+  const num = input.value;
   input.value = "";
   input.focus();
-  para.textContent = "";
-  for (const contact of contacts) {
-    const splitContact = contact.split(":");
-    if (splitContact[0].toLowerCase() === searchName) {
-      para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}`;
-      break;
+  for (let i = 1; i <= num; i++) {
+    let sqRoot = Math.sqrt(i);
+    if (Math.floor(sqRoot) !== Math.sqrt(i)) {
+      continue;
     }
-  }
-  if (para.textContent === "") {
-    para.textContent = "contact not found";
+    para.textContent += `${i} `;
   }
 });
